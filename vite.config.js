30b85fa7b4e553226b6d31f975ca4f6648c8cca5
k224041-kazui <k224t041@st.kyoto-seika.ac.jp>
2026-07-16 15:27:20 +0900

    fix: base設定とrollupOptionsを修正

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/my-website/',
   build: {
+    outDir: 'docs',
     rollupOptions: {
       input: {
-        main: resolve(import.meta.dirname, 'index.html'),
-        works: resolve(import.meta.dirname, 'works.html'),
+        main: resolve(__dirname, 'index.html'),
+        works: resolve(__dirname, 'works.html'),
       },
     },
   },
-});
+})
